4352bc3c76ecb6606faf615955ebb9d3b187ddd0
Mykhailo Parfeniuk <sopyer@gmail.com>
2021-10-27 16:13:20 +0300

    TheForge source integration - sources modified, directory structure changed. Direct integration will fail.

---
+++ Code/System/Render/TheForge/Interfaces/IOperatingSystem.h
@@ -0,0 +1,273 @@
+/*
+ * Copyright (c) 2018-2021 The Forge Interactive Inc.
+ *
+ * This file is part of The-Forge
+ * (see https://github.com/ConfettiFX/The-Forge).
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+*/
+
+#pragma once
+
+#include "../Core/Config.h"
+
+#if defined(_WINDOWS) || defined(XBOX)
+#include <sys/stat.h>
+#include <stdlib.h>
+#include <windows.h>
+#endif
+
+#if defined(__APPLE__)
+#if !defined(TARGET_IOS)
+#import <Carbon/Carbon.h>
+#else
+#include <stdint.h>
+typedef uint64_t uint64;
+#endif
+#elif defined(__ANDROID__)
+#include <android_native_app_glue.h>
+#include <android/log.h>
+#elif defined(__linux__) && !defined(VK_USE_PLATFORM_GGP)
+#define VK_USE_PLATFORM_XLIB_KHR
+#if defined(VK_USE_PLATFORM_XLIB_KHR) || defined(VK_USE_PLATFORM_XCB_KHR)
+#include <X11/Xutil.h>
+// X11 defines primitive types which conflict with Forge libraries
+#undef Bool
+#endif
+#elif defined(NX64)
+#include "../../../Switch/Common_3/NX/NXTypes.h"
+#endif
+
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+
+// For time related functions such as getting localtime
+#include <time.h>
+
+#include <float.h>
+#include <limits.h>
+#include <stddef.h>
+#include <stdbool.h>
+
+#define IMEMORY_FROM_HEADER
+#include "../Interfaces/IMemory.h"
+
+#if !defined(_WINDOWS) && !defined(XBOX)
+#include <unistd.h>
+#define stricmp(a, b) strcasecmp(a, b)
+#if !defined(ORBIS) && !defined(PROSPERO)
+#define vsprintf_s vsnprintf
+#endif
+#endif
+
+#if defined(XBOX)
+#define stricmp(a, b) _stricmp(a, b)
+#endif
+
+typedef struct WindowHandle
+{
+// TODO: Separate vulkan ext from choosing xlib vs xcb
+#if defined(VK_USE_PLATFORM_XLIB_KHR)
+	Display* display;
+	Window   window;
+	Atom     xlib_wm_delete_window;
+	Colormap colormap;
+#elif defined(VK_USE_PLATFORM_XCB_KHR)
+	xcb_connection_t*        connection;
+	xcb_window_t             window;
+	xcb_screen_t*            screen;
+	xcb_intern_atom_reply_t* atom_wm_delete_window;
+#elif defined(__ANDROID__)
+	ANativeWindow*           window;
+	ANativeActivity*         activity;
+	AConfiguration*			 configuration;
+#else
+	void* window;    //hWnd
+#endif
+} WindowHandle;
+
+typedef struct RectDesc
+{
+	int32_t left;
+	int32_t top;
+	int32_t right;
+	int32_t bottom;
+} RectDesc;
+
+inline int getRectWidth(const RectDesc* rect) { return rect->right - rect->left; }
+
+inline int getRectHeight(const RectDesc* rect) { return rect->bottom - rect->top; }
+
+typedef struct
+{
+	WindowHandle handle;
+	RectDesc     windowedRect;
+	RectDesc     fullscreenRect;
+	RectDesc     clientRect;
+	uint32_t     windowsFlags;
+	bool         fullScreen;
+	bool         cursorTracked;
+	bool         iconified;
+	bool         maximized;
+	bool         minimized;
+	bool         hide;
+	bool         noresizeFrame;
+	bool         borderlessWindow;
+	bool         overrideDefaultPosition;
+	bool         centered;
+	bool         forceLowDPI;
+} WindowsDesc;
+
+typedef struct Resolution
+{
+	uint32_t mWidth;
+	uint32_t mHeight;
+} Resolution;
+
+// Monitor data
+//
+typedef struct
+{
+	RectDesc     monitorRect;
+	RectDesc     workRect;
+	unsigned int dpi[2];
+	unsigned int physicalSize[2];
+	// This size matches the static size of DISPLAY_DEVICE.DeviceName
+#if defined(_WINDOWS) || defined(XBOX)
+	WCHAR adapterName[32];
+	WCHAR displayName[32];
+	WCHAR publicAdapterName[128];
+	WCHAR publicDisplayName[128];
+#elif defined(__APPLE__)
+#if defined(TARGET_IOS)
+#else
+	CGDirectDisplayID displayID;
+	char              publicAdapterName[64];
+	char              publicDisplayName[64];
+#endif
+#elif defined(__linux__) && !defined(__ANDROID__)
+	Screen*          screen;
+	char             adapterName[32];
+	char             displayName[32];
+	char             publicAdapterName[64];
+	char             publicDisplayName[64];
+#else
+	char  adapterName[32];
+	char  displayName[32];
+	char  publicAdapterName[64];
+	char  publicDisplayName[64];
+#endif
+	Resolution* resolutions;
+	Resolution  defaultResolution;
+	uint32_t    resolutionCount;
+	bool        modesPruned;
+	bool        modeChanged;
+} MonitorDesc;
+
+#if defined(_WINDOWS)
+typedef enum ResetScenario
+{
+
+	RESET_SCENARIO_NONE = 0x0,
+	RESET_SCENARIO_RELOAD = 0x1,
+	RESET_SCENARIO_DEVICE_LOST = 0x2,
+	RESET_SCENARIO_API_SWITCH = 0x4,
+	RESET_SCENARIO_GPU_MODE_SWITCH = 0x8,
+
+} ResetScenario;
+
+void onRequestReload();
+void onDeviceLost();
+void onGpuModeSwitch();
+#elif defined(__ANDROID__)
+typedef enum ResetScenario
+{
+
+	RESET_SCENARIO_NONE = 0x0,
+	RESET_SCENARIO_RELOAD = 0x1,
+	RESET_SCENARIO_API_SWITCH = 0x2,
+
+} ResetScenario;
+
+void onRequestReload();
+void onDeviceLost();
+void onAPISwitch();
+#else
+typedef enum ResetScenario
+{
+
+	RESET_SCENARIO_NONE = 0x0,
+	RESET_SCENARIO_RELOAD = 0x1,
+
+} ResetScenario;
+
+void onRequestReload(void);
+void onDeviceLost(void);
+void onAPISwitch(void);
+#endif
+
+// API functions
+void requestShutdown(void);
+void errorMessagePopup(const char* title, const char* msg, void* windowHandle);
+
+// Custom processing of OS pipe messages
+typedef int32_t (*CustomMessageProcessor)(WindowsDesc* pWindow, void* msg);
+void setCustomMessageProcessor(CustomMessageProcessor proc);
+
+// Window handling
+void openWindow(const char* app_name, WindowsDesc* winDesc);
+void closeWindow(const WindowsDesc* winDesc);
+void setWindowRect(WindowsDesc* winDesc, const RectDesc* rect);
+void setWindowSize(WindowsDesc* winDesc, unsigned width, unsigned height);
+void toggleBorderless(WindowsDesc* winDesc, unsigned width, unsigned height);
+void toggleFullscreen(WindowsDesc* winDesc);
+void showWindow(WindowsDesc* winDesc);
+void hideWindow(WindowsDesc* winDesc);
+void maximizeWindow(WindowsDesc* winDesc);
+void minimizeWindow(WindowsDesc* winDesc);
+void centerWindow(WindowsDesc* winDesc);
+
+// Mouse and cursor handling
+void* createCursor(const char* path);
+void  setCursor(void* cursor);
+void  showCursor(void);
+void  hideCursor(void);
+bool  isCursorInsideTrackingArea(void);
+void  setMousePositionRelative(const WindowsDesc* winDesc, int32_t x, int32_t y);
+void  setMousePositionAbsolute(int32_t x, int32_t y);
+
+void getRecommendedResolution(RectDesc* rect);
+// Sets video mode for specified display
+void setResolution(const MonitorDesc* pMonitor, const Resolution* pRes);
+
+MonitorDesc* getMonitor(uint32_t index);
+uint32_t     getMonitorCount(void);
+// pArray pointer to array with at least 2 elements(x,y)
+void getDpiScale(float array[2]);
+
+bool getResolutionSupport(const MonitorDesc* pMonitor, const Resolution* pRes);
+
+// Shell commands
+
+/// @param stdOutFile The file to which the output of the command should be written. May be NULL.
+int systemRun(const char* command, const char** arguments, size_t argumentCount, const char* stdOutFile);
+
+//
+// failure research ...
+//
---
+++ Code/System/Render/TheForge/Windows/WindowsFileSystem.cpp
@@ -0,0 +1,244 @@
+/*
+ * Copyright (c) 2018-2021 The Forge Interactive Inc.
+ *
+ * This file is part of The-Forge
+ * (see https://github.com/ConfettiFX/The-Forge).
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+*/
+
+#include <malloc.h>
+#include "../Core/Config.h"
+
+#include <functional>
+
+#if !defined(XBOX)
+#include "shlobj.h"
+#include "commdlg.h"
+#include <WinBase.h>
+#endif
+
+#include "../Interfaces/ILog.h"
+#include "../Interfaces/IOperatingSystem.h"
+#include "../Interfaces/IThread.h"
+#include "../Interfaces/IMemory.h"
+
+template <typename T>
+static inline T withUTF16Path(const char* path, T(*function)(const wchar_t*))
+{
+	size_t len = strlen(path);
+	wchar_t* buffer = (wchar_t*)alloca((len + 1) * sizeof(wchar_t));
+
+	size_t resultLength = MultiByteToWideChar(CP_UTF8, 0, path, (int)len, buffer, (int)len);
+	buffer[resultLength] = 0;
+
+	return function(buffer);
+}
+
+
+
+#ifndef XBOX
+static bool gInitialized = false;
+static const char* gResourceMounts[RM_COUNT];
+const char* getResourceMount(ResourceMount mount) {
+	return gResourceMounts[mount];
+}
+
+static char gApplicationPath[FS_MAX_PATH] = {};
+static char gDocumentsPath[FS_MAX_PATH] = {};
+
+bool initFileSystem(FileSystemInitDesc* pDesc)
+{
+	if (gInitialized)
+	{
+		LOGF(LogLevel::eWARNING, "FileSystem already initialized.");
+		return true;
+	}
+	ASSERT(pDesc);
+	pSystemFileIO->GetResourceMount = getResourceMount;
+
+	for (uint32_t i = 0; i < RM_COUNT; ++i)
+		gResourceMounts[i] = "";
+
+	// Get application directory
+	wchar_t utf16Path[FS_MAX_PATH];
+	GetModuleFileNameW(0, utf16Path, FS_MAX_PATH);
+	char applicationFilePath[FS_MAX_PATH] = {};
+	WideCharToMultiByte(CP_UTF8, 0, utf16Path, -1, applicationFilePath, MAX_PATH, NULL, NULL);
+	fsGetParentPath(applicationFilePath, gApplicationPath);
+	gResourceMounts[RM_CONTENT] = gApplicationPath;
+	gResourceMounts[RM_DEBUG] = gApplicationPath;
+
+	// Get user directory
+	PWSTR userDocuments = NULL;
+	SHGetKnownFolderPath(FOLDERID_Documents, 0, NULL, &userDocuments);
+	WideCharToMultiByte(CP_UTF8, 0, userDocuments, -1, gDocumentsPath, MAX_PATH, NULL, NULL);
+	CoTaskMemFree(userDocuments);
+	gResourceMounts[RM_DOCUMENTS] = gDocumentsPath;
+	gResourceMounts[RM_SAVE_0] = gApplicationPath;
+
+	// Override Resource mounts
+	for (uint32_t i = 0; i < RM_COUNT; ++i)
+	{
+		if (pDesc->pResourceMounts[i])
+			gResourceMounts[i] = pDesc->pResourceMounts[i];
+	}
+
+	//// Get app data directory
+	//char appData[FS_MAX_PATH] = {};
+	//PWSTR localAppdata = NULL;
+	//SHGetKnownFolderPath(FOLDERID_LocalAppData, 0, NULL, &localAppdata);
+	//pathLength = wcslen(localAppdata);
+	//utf8Length = WideCharToMultiByte(CP_UTF8, 0, localAppdata, (int)pathLength, NULL, 0, NULL, NULL);
+	//WideCharToMultiByte(CP_UTF8, 0, localAppdata, (int)pathLength, appData, utf8Length, NULL, NULL);
+	//CoTaskMemFree(localAppdata);
+
+	gInitialized = true;
+	return true;
+}
+
+void exitFileSystem(void)
+{
+	gInitialized = false;
+}
+#endif
+
+static bool fsDirectoryExists(const char* path)
+{
+	return withUTF16Path<bool>(path, [](const wchar_t* pathStr)
+	{
+		DWORD attributes = GetFileAttributesW(pathStr);
+		return (attributes != INVALID_FILE_ATTRIBUTES && (attributes & FILE_ATTRIBUTE_DIRECTORY));
+	});
+}
+
+static bool fsCreateDirectory(const char* path)
+{
+	if (fsDirectoryExists(path))
+	{
+		return true;
+	}
+
+	char parentPath[FS_MAX_PATH] = { 0 };
+	fsGetParentPath(path, parentPath);
+	if (parentPath[0] != 0)
+	{
+		if (!fsDirectoryExists(parentPath))
+		{
+			fsCreateDirectory(parentPath);
+		}
+	}
+	return withUTF16Path<bool>(path, [](const wchar_t* pathStr)
+	{
+		return ::CreateDirectoryW(pathStr, NULL) ? true : false;
+	});
+}
+
+bool fsCreateDirectory(ResourceDirectory resourceDir)
+{
+	return fsCreateDirectory(fsGetResourceDirectory(resourceDir));
+}
+
+time_t fsGetLastModifiedTime(ResourceDirectory resourceDir, const char* fileName)
+{
+	const char* resourcePath = fsGetResourceDirectory(resourceDir);
+	char filePath[FS_MAX_PATH] = { 0 };
+	fsAppendPathComponent(resourcePath, fileName, filePath);
+
+	// Fix paths for Windows 7 - needs to be generalized and propagated
+	//eastl2::string path = eastl2::string(filePath);
+	//auto directoryPos = path.find(":");
+	//eastl2::string cleanPath = path.substr(directoryPos - 1);
+
+	struct stat fileInfo = { 0 };
+	stat(filePath, &fileInfo);
+	return fileInfo.st_mtime;
+}
+
+bool PlatformOpenFile(ResourceDirectory resourceDir, const char* fileName, FileMode mode, FileStream* pOut)
+{
+	const char* resourcePath = fsGetResourceDirectory(resourceDir);
+	char filePath[FS_MAX_PATH] = {};
+	fsAppendPathComponent(resourcePath, fileName, filePath);
+
+	// Path utf-16 conversion
+	size_t filePathLen = strlen(filePath);
+	wchar_t* pathStr = (wchar_t*)alloca((filePathLen + 1) * sizeof(wchar_t));
+	size_t pathStrLength =
+		MultiByteToWideChar(CP_UTF8, 0, filePath, (int)filePathLen, pathStr, (int)filePathLen);
+	pathStr[pathStrLength] = 0;
+
+	// Mode string utf-16 conversion
+	const char* modeStr = fsFileModeToString(mode);
+
+	wchar_t modeWStr[4] = {};
+	mbstowcs(modeWStr, modeStr, 4);
+
+	FILE* fp = NULL;
+	if (mode & FM_ALLOW_READ)
+	{
+		fp = _wfsopen(pathStr, modeWStr, _SH_DENYWR);
+	}
+	else
+	{
+		_wfopen_s(&fp, pathStr, modeWStr);
+	}
+
+	// We need to change mode for read | write mode to 'w+' or 'wb+'
+	// if file doesn't exist so that it can be created
+	if (!fp)
+	{
+		// Try changing mode to 'w+' or 'wb+'
+		if ((mode & FM_READ_WRITE) == FM_READ_WRITE)
+		{
+			modeStr = fsOverwriteFileModeToString(mode);
+			mbstowcs(modeWStr, modeStr, 4);
+			if (mode & FM_ALLOW_READ)
+			{
+				fp = _wfsopen(pathStr, modeWStr, _SH_DENYWR);
+			}
+			else
+			{
+				_wfopen_s(&fp, pathStr, modeWStr);
+			}
+		}
+	}
+
+	if (fp)
+	{
+		*pOut = {};
+		pOut->pFile = fp;
+		pOut->mMode = mode;
+		pOut->pIO = pSystemFileIO;
+
+		pOut->mSize = -1;
+		if (fseek(pOut->pFile, 0, SEEK_END) == 0)
+		{
+			pOut->mSize = ftell(pOut->pFile);
+			rewind(pOut->pFile);
+		}
+
+		return true;
+	}
+	else
+	{
+		LOGF(LogLevel::eERROR, "Error opening file: %s -- %s (error: %s)", filePath, modeStr, strerror(errno));
+	}
+
+	return false;
+}
